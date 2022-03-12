a703c32416b6f0802abca42e054b655d4747b64b
吴大帅 <2020011028@secoder.net>
2022-03-13 02:50:49 +0800

    fix:更改 secoder/settings.py，允许域名访问

--- secoder/settings.py
+++ secoder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#rk4h^d7)d(dvyfmc5^5ebq*la^e$$5%mb1=aia^&i#vlswfjf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
