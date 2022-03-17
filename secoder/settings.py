a57c0bb8cba78877fa2db109a74d29e7d4e2ccb5
吴大帅 <2020011028@secoder.net>
2022-03-17 13:55:29 +0000

    feat:将数据库地址迁移至持久卷

--- secoder/settings.py
+++ secoder/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'secoder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR /'django_storage'/ 'db.sqlite3',
     }
 }
 
