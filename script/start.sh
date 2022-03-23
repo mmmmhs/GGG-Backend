50315a119b2987c470083fccbac7545e0740f226
吴大帅 <2020011028@secoder.net>
2022-03-23 16:43:26 +0000

    fix:修正部署脚本

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,12 @@
 python manage.py makemigrations
+if [ $? -ne 0 ]; then
+rm -rf /opt/secoder/django_storage/*
+python manage.py makemigrations
+fi
 python manage.py migrate
+if [ $? -ne 0 ]; then
+rm -rf /opt/secoder/django_storage/*
+python manage.py makemigrations
+python manage.py migrate
+fi
 daphne secoder.asgi:application -b 0.0.0.0 -p  $BIND
