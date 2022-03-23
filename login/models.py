38dbc94e75b54781f6bd6d87bf0aa799d4c79214
吴大帅 <2020011028@secoder.net>
2022-03-23 19:13:34 +0800

    fix:优化性能，修复sess行为

--- login/models.py
+++ login/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class Passenger(models.Model):
     name = models.CharField(unique=True, max_length=500)
 
+
 class Driver(models.Model):
-    name = models.CharField(unique=True,max_length=500)
+    name = models.CharField(unique=True, max_length=500)
+
 
 class SessionId(models.Model):
-    sessId = models.CharField(unique=True, max_length=500) #加密后的sessionID
-    key = models.CharField(max_length=500) #解密key
-    username = models.CharField(unique=True, max_length=500) #openID
+    sessId = models.CharField(max_length=500)  # 加密后的sessionID
+    username = models.CharField(unique=True, max_length=500)  # openID
     job = models.CharField(max_length=100)
--- login/views.py
+++ login/views.py
@@ -8,51 +8,61 @@ from django.http import HttpResponse, JsonResponse
 import requests
 from login.models import Driver, Passenger, SessionId
 import secoder.settings
-from cryptography.fernet import Fernet
+import random
+import string
+import hashlib
+
 
 def index(request):
     return HttpResponse("Hello world.")
 
+
 def get_3rd_session(session_key, openId, job):
-    key = Fernet.generate_key()
-    cipher = Fernet(key)
-    encrypted_data = cipher.encrypt(session_key.encode('utf-8'))
-    SessionId.objects.create(sessId = encrypted_data, key = key, username = openId, job = job)
+    data = session_key+openId+job
+    data += ''.join(random.sample(string.ascii_letters + string.digits, 8))
+    encrypted_data = hashlib.new(
+        'md5', bytes(data, encoding="utf8")).hexdigest()
     return encrypted_data
 
+
 def login(request):
-    if(request.method =='GET'):
-        code = request.GET.get('code',default = '')
-        job = request.GET.get('job',default = '')
-        response = requests.get("https://api.weixin.qq.com/sns/jscode2session?appid="+secoder.settings.APPID+"&secret="+secoder.settings.APPSECRET+"&js_code="+code+"&grant_type=authorization_code")
+    if(request.method == 'GET'):
+        code = request.GET.get('code', default='')
+        job = request.GET.get('job', default='')
+        response = requests.get("https://api.weixin.qq.com/sns/jscode2session?appid="+secoder.settings.APPID +
+                                "&secret="+secoder.settings.APPSECRET+"&js_code="+code+"&grant_type=authorization_code")
         data = response.json()
         try:
-            openID = data['openId']
+            openID = data['openid']
             sessionID = get_3rd_session(data['session_key'], openID, job)
-            errorcode = data['errorcode']
-            if job =='passenger':
-                user = Passenger.objects.filter(name = openID).first()
-                if not user and errorcode == 0:
-                    errorcode = -10
-            if job =='driver':
-                user = Driver.objects.filter(name = openID).first()
-                if not user and errorcode == 0:
-                    errorcode = -10
-            return JsonResponse({'errorcode':errorcode,'sess':sessionID})
+            try:
+                errorcode = data['errcode']
+            except Exception:
+                errorcode = 0
+            if errorcode != 0:
+                return JsonResponse({'errcode': errorcode, 'sess': ""})
+            if job == 'passenger':
+                user = Passenger.objects.filter(name=openID).first()
+            elif job == 'driver':
+                user = Driver.objects.filter(name=openID).first()
+            if not user:
+                errorcode = -10
+            SessionId.objects.update_or_create(username=openID,defaults={"sessId":"sessionID","job":"job"})
+            return JsonResponse({'errcode': errorcode, 'sess': sessionID})
         except Exception as e:
             return HttpResponse("error:{}".format(e), status=405)
 
+
 def reg(request):
-    if(request.method =='GET'):
+    if(request.method == 'GET'):
         try:
-            sess = request.GET.get('sess',default = '')
-            user = SessionId.objects.filter(sessId = sess).first()
+            sess = request.GET.get('sess', default='')
+            user = SessionId.objects.filter(sessId=sess).first()
             if(user.job == "passenger"):
-                Passenger.objects.create(name = user.username)
+                Passenger.objects.create(name=user.username)
             elif(user.job == "driver"):
-                Driver.objects.create(name = user.username)
-            return JsonResponse({'errorcode':1})
-        except Exception:
-            return JsonResponse({'errorcode':-2})
-    
-    
+                Driver.objects.create(name=user.username)
+            return JsonResponse({'errcode': 1})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'errcode': -2})
