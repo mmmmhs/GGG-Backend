b3e4887e3ebad3c84d4a7b5f616c4ca495547011
吴大帅 <2020011028@secoder.net>
2022-04-06 18:45:57 +0000

    feat:创建超级用户

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,8 @@
+ENV DJANGO_DB_NAME=default
+ENV DJANGO_SU_NAME=admin
+ENV DJANGO_SU_EMAIL=admin@my.company
+ENV DJANGO_SU_PASSWORD=adminpass
+
 python manage.py makemigrations
 if [ $? -ne 0 ]; then
 rm -rf /opt/secoder/django_storage/*
@@ -8,5 +13,13 @@ if [ $? -ne 0 ]; then
 rm -rf /opt/secoder/django_storage/*
 python manage.py makemigrations
 python manage.py migrate
+
 fi
+
+RUN python -c "import django; django.setup(); \
+   from django.contrib.auth.management.commands.createsuperuser import get_user_model; \
+   get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser( \
+   username='$DJANGO_SU_NAME', \
+   email='$DJANGO_SU_EMAIL', \
+   password='$DJANGO_SU_PASSWORD')"
 daphne secoder.asgi:application -b 0.0.0.0 -p  $BIND
