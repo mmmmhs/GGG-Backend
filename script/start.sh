fdf6a9dc51b569ffda2a46b06ae6f396c70e0e3f
吴大帅 <2020011028@secoder.net>
2022-04-07 02:18:07 +0000

    fix:修复部署脚本

--- script/start.sh
+++ script/start.sh
@@ -1,18 +1,8 @@
-ENV  DJANGO_SUPERUSER_NAME =admin
-ENV  DJANGO_SUPERUSER_EMAIL =admin@my.company
-ENV  DJANGO_SUPERUSER_PASSWORD =adminpass
+export  DJANGO_SUPERUSER_NAME ="admin"
+export  DJANGO_SUPERUSER_EMAIL ="admin@my.company"
+export  DJANGO_SUPERUSER_PASSWORD ="adminpass"
 
 python manage.py makemigrations
-if [ $? -ne 0 ]; then
-rm -rf /opt/secoder/django_storage/*
-python manage.py makemigrations
-fi
 python manage.py migrate
-if [ $? -ne 0 ]; then
-rm -rf /opt/secoder/django_storage/*
-python manage.py makemigrations
-python manage.py migrate
-
-fi
 python manage.py createsuperuser --noinput
 daphne secoder.asgi:application -b 0.0.0.0 -p  $BIND
